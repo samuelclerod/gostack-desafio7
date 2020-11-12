46d3579d1505a6a3377766955cd1df5810e7aae6
Samuel Rodrigues <samuelclerod@gmail.com>
2020-11-12 01:28:31 -0300

    Challenge finished

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach((fileProps: FileProps) =>
+      data.append('file', fileProps.file, fileProps.name),
+    );
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
     // TODO
+    setUploadedFiles(
+      files.map(
+        (file: File): FileProps => ({
+          file,
+          name: file.name,
+          readableSize: String(file.size),
+        }),
+      ),
+    );
   }
 
   return (
